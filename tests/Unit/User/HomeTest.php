08668b38e1b8deeb62601f14d5e289dcce138b3f
hadymaher31 <hadymaher31@yahoo.com>
2019-12-12 13:08:53 +0200

    add user logged in test

--- tests/Unit/User/HomeTest.php
+++ tests/Unit/User/HomeTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\User;
 use Tests\TestCase;
 use App\Model\user\tag;
 use App\Model\user\post;
+use App\Model\user\User;
 use App\Model\user\category;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -25,16 +26,51 @@ class HomeTest extends TestCase
 
     public function test_tag_page_without_login()
     {
-        $tag = tag::find(1);
+        $tag = tag::first();
         $response = $this->get('/post/tag/' . $tag->slug);
 
         $response->assertStatus(200);
     }
-    
+
     public function test_category_page_without_login()
     {
-        $category = category::find(1);
+        $category = category::first();
         $response = $this->get('/post/category/' . $category->slug);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_home_page_responce_with_login()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_tag_page_with_login()
+    {
+        $tag = tag::first();
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->get('/post/tag/' . $tag->slug);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_category_page_with_login()
+    {
+        $category = category::first();
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->get('/post/category/' . $category->slug);
 
         $response->assertStatus(200);
     }
