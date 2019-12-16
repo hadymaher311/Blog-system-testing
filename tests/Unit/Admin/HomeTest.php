61c05ce26fb2c66bbec4d4dde22413050cb4893d
hadymaher31 <hadymaher31@yahoo.com>
2019-12-16 23:12:00 +0200

    add admin home test

---
+++ tests/Unit/Admin/HomeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit\Admin;
+
+use Tests\TestCase;
+use App\Model\admin\admin;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class HomeTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_admin_home_page_responce_without_login()
+    {
+        $response = $this->get('admin/home');
+        $response->assertRedirect('/admin-login');
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_admin_home_page_responce_with_login()
+    {
+        $admin = factory(admin::class)->create();
+
+        $response = $this->actingAs($admin, 'admin')
+            ->withSession(['foo' => 'bar'])->get('admin/home');
+        $response->assertOk();
+    }
+}
