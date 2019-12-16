cf2544582edd0e42eccc4f7279e45989e53195ce
hadymaher31 <hadymaher31@yahoo.com>
2019-12-16 02:34:39 +0200

    add login test units

---
+++ tests/Unit/User/LoginTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit\User;
+
+use Tests\TestCase;
+use App\Model\user\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class LoginTest extends TestCase
+{
+    /**
+     * The login form can be displayed.
+     *
+     * @return void
+     */
+    public function testLoginFormDisplayed()
+    {
+        $response = $this->get('/login');
+        $response->assertStatus(200);
+    }
+    /**
+     * A valid user can be logged in.
+     *
+     * @return void
+     */
+    public function testLoginAValidUser()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'secret'
+        ]);
+        $response->assertStatus(302);
+        $this->assertAuthenticatedAs($user);
+    }
+    /**
+     * An invalid user cannot be logged in.
+     *
+     * @return void
+     */
+    public function testDoesNotLoginAnInvalidUser()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid'
+        ]);
+        $response->assertSessionHasErrors();
+        $this->assertGuest();
+    }
+    /**
+     * A logged in user can be logged out.
+     *
+     * @return void
+     */
+    public function testLogoutAnAuthenticatedUser()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->post('/logout');
+        $response->assertStatus(302);
+        $this->assertGuest();
+    }
+}
---
+++ tests/Unit/User/RegisterTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Unit\User;
+
+use Tests\TestCase;
+use App\Model\user\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RegisterTest extends TestCase
+{
+    /**
+     * The registration form can be displayed.
+     *
+     * @return void
+     */
+    public function testRegisterFormDisplayed()
+    {
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+    }
+    /**
+     * A valid user can be registered.
+     *
+     * @return void
+     */
+    public function testRegistersAValidUser()
+    {
+        $user = factory(User::class)->make();
+        $response = $this->post('register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ]);
+        $response->assertStatus(302);
+        $this->assertAuthenticated();
+    }
+    /**
+     * An invalid user is not registered.
+     *
+     * @return void
+     */
+    public function testDoesNotRegisterAnInvalidUser()
+    {
+        $user = factory(User::class)->make();
+        $response = $this->post('register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'password',
+            'password_confirmation' => 'invalid'
+        ]);
+        $response->assertSessionHasErrors();
+        $this->assertGuest();
+    }
+}
